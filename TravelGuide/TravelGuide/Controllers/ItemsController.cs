a3ffd70479f0b24465dff20dd392c72731fd21da
ahami14 <ahnikkahamilton14@gmail.com>
2019-11-20 15:21:13 -0600

    Itinerary and ItemsInItinerary controller added with logic

---
+++ TravelGuide/TravelGuide/Controllers/ItemsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelGuide.Models;
+
+namespace TravelGuide.Controllers
+{
+    public class ItemsController : Controller
+    {
+        ApplicationDbContext context;
+        public ItemsController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Items
+        public ActionResult Index()
+        {
+            var items = context.Items;
+            return View(items);
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int id)
+        {
+            ItemInItinerary item = context.Items.Where(i => i.ItemId == id).FirstOrDefault();
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            ItemInItinerary item = new ItemInItinerary();
+            return View();
+        }
+
+        // POST: Items/Create
+        [HttpPost]
+        public ActionResult Create(ItemInItinerary item)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Items.Add(item);
+                context.SaveChanges();
+                var itinerary = context.Itineraries.Where(i => i.ItineraryId == item.ItemId).FirstOrDefault();
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ItemInItinerary item = context.Items.Where(i => i.ItemId == id).FirstOrDefault();
+            return View(item);
+        }
+
+        // POST: Items/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, ItemInItinerary item)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                ItemInItinerary itemToEdit = context.Items.Where(i => i.ItemId == id).FirstOrDefault();
+                itemToEdit.DateOfEvent = item.DateOfEvent;
+                itemToEdit.DetailsOfEvent = item.DetailsOfEvent;
+                itemToEdit.EventName = item.EventName;
+                itemToEdit.LocationOfEvent = item.LocationOfEvent;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(int id)
+        {
+            ItemInItinerary item = context.Items.Where(i => i.ItemId == id).FirstOrDefault();
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, ItemInItinerary item)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                ItemInItinerary itemToDelete = context.Items.Where(i => i.ItemId == id).FirstOrDefault();
+                context.Items.Remove(itemToDelete);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TravelGuide/TravelGuide/Controllers/ItineraryController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelGuide.Models;
+
+namespace TravelGuide.Controllers
+{
+    public class ItineraryController : Controller
+    {
+        ApplicationDbContext context;
+        public ItineraryController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Itinerary
+        public ActionResult Index()
+        {
+            var itineraries = context.Itineraries;
+            return View(itineraries);
+        }
+
+        // GET: Itinerary/Details/5
+        public ActionResult Details(int id)
+        {
+            Itinerary itinerary = context.Itineraries.Where(i => i.ItineraryId == id).FirstOrDefault();
+            return View(itinerary);
+        }
+
+        // GET: Itinerary/Create
+        public ActionResult Create()
+        {
+            Itinerary itinerary = new Itinerary();
+            return View();
+        }
+
+        // POST: Itinerary/Create
+        [HttpPost]
+        public ActionResult Create(Itinerary itinerary)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Itineraries.Add(itinerary);
+                context.SaveChanges();
+                string id = User.Identity.GetUserId();
+                var user = context.Customers.Where(u => u.ApplicationId == id).FirstOrDefault();
+                itinerary.ItineraryId = user.CustomerId;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Itinerary/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Itinerary itinerary = context.Itineraries.Where(i => i.ItineraryId == id).FirstOrDefault();
+            return View(itinerary);
+        }
+
+        // POST: Itinerary/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Itinerary itinerary)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Itinerary editItinerary = context.Itineraries.Where(i => i.ItineraryId == id).FirstOrDefault();
+                editItinerary.ItineraryName = itinerary.ItineraryName;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Itinerary/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Itinerary itinerary = context.Itineraries.Where(i => i.ItineraryId == id).FirstOrDefault();
+            return View(itinerary);
+        }
+
+        // POST: Itinerary/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Itinerary itinerary)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Itinerary itineraryToDelete = context.Itineraries.Where(i => i.ItineraryId == id).FirstOrDefault();
+                context.Itineraries.Remove(itineraryToDelete);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
